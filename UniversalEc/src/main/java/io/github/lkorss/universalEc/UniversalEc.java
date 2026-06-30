9db1d8d75ec7e09082be7dfe35fb8366a7a90ede
lkorss <lgbotkinmark@gmail.com>
2026-06-30 14:51:13 +0500

    Add 1.0 version
    There may be bugs with placement and other issues

---
+++ UniversalEc/src/main/java/io/github/lkorss/universalEc/UniversalEc.java
@@ -0,0 +1,75 @@
+package io.github.lkorss.universalEc;
+
+import io.github.lkorss.universalEc.commands.UnecTabCompleter;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import io.github.lkorss.universalEc.commands.EcCommand;
+import io.github.lkorss.universalEc.utils.MessageManager;
+import io.github.lkorss.universalEc.listeners.EnderChestClickListener;
+import io.github.lkorss.universalEc.commands.UnecCommand;
+
+import java.io.File;
+
+public final class UniversalEc extends JavaPlugin {
+
+    private MessageManager messageManager;
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        saveDefaultLangFiles();
+
+        this.messageManager = new MessageManager(this);
+
+        // Регистрация команды для игроков
+        if (getCommand("ec") != null) {
+            getCommand("ec").setExecutor(new EcCommand(this));
+        }
+
+
+        if (getCommand("unec") != null) {
+            getCommand("unec").setExecutor(new UnecCommand(this));
+            getCommand("unec").setTabCompleter(new UnecTabCompleter());
+        }
+
+        getServer().getPluginManager().registerEvents(new EnderChestClickListener(this), this);
+    }
+
+    public void reloadPluginConfig() {
+        reloadConfig();
+        this.messageManager.loadLang();
+    }
+
+    public void playEnderChestSound(Player player) {
+        if (!getConfig().getBoolean("sound.enabled", true)) {
+            return;
+        }
+
+        String soundName = getConfig().getString("sound.name", "BLOCK_ENDER_CHEST_OPEN");
+        if (soundName.equalsIgnoreCase("NONE")) {
+            return;
+        }
+
+        try {
+            Sound sound = Sound.valueOf(soundName.toUpperCase());
+            float volume = (float) getConfig().getDouble("sound.volume", 1.0);
+            float pitch = (float) getConfig().getDouble("sound.pitch", 1.0);
+
+            player.playSound(player.getLocation(), sound, volume, pitch);
+        } catch (IllegalArgumentException e) {
+            getLogger().warning("Failed to play sound! Check sound.name in config.yml. Error: " + soundName);
+        }
+    }
+
+    private void saveDefaultLangFiles() {
+        File ruFile = new File(getDataFolder(), "lang/ru.yml");
+        File enFile = new File(getDataFolder(), "lang/en.yml");
+        if (!ruFile.exists()) saveResource("lang/ru.yml", false);
+        if (!enFile.exists()) saveResource("lang/en.yml", false);
+    }
+
+    public MessageManager getMessageManager() {
+        return messageManager;
+    }
+}
---
+++ UniversalEc/src/main/java/io/github/lkorss/universalEc/commands/EcCommand.java
@@ -0,0 +1,46 @@
+package io.github.lkorss.universalEc.commands;
+
+import io.github.lkorss.universalEc.UniversalEc;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class EcCommand implements CommandExecutor {
+
+    private final UniversalEc plugin;
+
+    public EcCommand(UniversalEc plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(plugin.getMessageManager().getMessage("only-players"));
+            return true;
+        }
+
+        Player player = (Player) sender;
+
+        if (!player.hasPermission("universalec.command.ec")) {
+            if (!plugin.getConfig().getBoolean("silent-mode.on-command-ec", false)) {
+                player.sendMessage(plugin.getMessageManager().getMessage("no-permission"));
+            }
+            return true;
+        }
+
+        if (!player.getInventory().contains(Material.ENDER_CHEST)) {
+            player.sendMessage(plugin.getMessageManager().getMessage("must-have-chest"));
+            return true;
+        }
+
+        player.openInventory(player.getEnderChest());
+        plugin.playEnderChestSound(player);
+        player.sendMessage(plugin.getMessageManager().getMessage("opened-via-command"));
+
+        return true;
+    }
+}
